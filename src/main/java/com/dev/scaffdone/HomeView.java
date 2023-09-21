dee6b9d3c6c0f10cfeb76c2b4c70b91504a409df
Owidiusz Zieliński <owi19955@gmail.com>
2023-09-21 21:57:27 +0200

    add components package to cleaning

--- src/main/java/com/dev/scaffdone/HomeView.java
+++ src/main/java/com/dev/scaffdone/HomeView.java
@@ -1,25 +1,21 @@
 package com.dev.scaffdone;
 
-import com.dev.scaffdone.scaffold.DialogView;
+
+import com.dev.scaffdone.components.HomeGrid;
 import com.dev.scaffdone.scaffold.ScaffoldService;
 import com.dev.scaffdone.scaffold.entity.CalculateModule;
 import com.dev.scaffdone.scaffold.entity.Scaffold;
 import com.dev.scaffdone.scaffold.entity.Size;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.CheckboxGroup;
 import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
-import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
@@ -35,15 +31,15 @@ import java.util.stream.IntStream;
 public class HomeView extends VerticalLayout implements AppShellConfigurator {
 
 
-
     public HomeView(ScaffoldService service) {
 
-        Grid<Scaffold> grid = createGrid();
-        final Scaffold owi = Scaffold.builder().id(1L).modules(List.of(new CalculateModule(Size.SIZE_073.getSize(),5)))
+        addClassName("home-view-h1-1");
+        HomeGrid grid = new HomeGrid();
+
+        final Scaffold owi = Scaffold.builder().id(1L).modules(List.of(new CalculateModule(Size.SIZE_073.getSize(), 5)))
                 .settled(true)
                 .additionalInfo("21JEDSAIODKJASKLDJSAKLDJASdasddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddKLDJKLSAJDSADJALSDK")
                 .username("Owi").height(10.0f).build();
-
 
 
         grid.setItems(
@@ -53,10 +49,12 @@ public class HomeView extends VerticalLayout implements AppShellConfigurator {
         //<theme-editor-local-classname>
         header.addClassName("home-view-h1-1");
 
+        header.addClassName("home-view-h1-1");
+
         VerticalLayout title = new VerticalLayout(header);
-        //<theme-editor-local-classname>
+
         title.addClassName("home-view-vertical-layout-1");
-        title.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+       
 
 
         add(
@@ -66,95 +64,19 @@ public class HomeView extends VerticalLayout implements AppShellConfigurator {
                         sizesBox(),
                         framesBoxes(),
                         height()),
-                        calculation()
-
+                    calculation()
         );
-
     }
 
-    private static Grid<Scaffold> createGrid() {
-        Grid<Scaffold> grid = new Grid<>(Scaffold.class, false);
-        grid.addColumn(Scaffold::getId).setHeader("Id");
-        grid.addColumn(Scaffold::getUsername).setHeader("User");
-        grid.addColumn(Scaffold::getModules).setHeader("Modules");
-        grid.addColumn(Scaffold::getFrameSize).setHeader("Frames");
-        grid.addColumn(Scaffold::getHeight).setHeader("Height");
-        grid.addColumn(Scaffold::getTotalLength).setHeader("Total Length");
-
-        grid.addComponentColumn(item -> {
-            Button button = new Button();
-            if (item.isSettled()) {
-                button.setText("YES");
-                button.getStyle().set("color", "white");
-                button.getStyle().set("background-color", "green");
-            } else {
-                button.setText("NO");
-                button.getStyle().set("background-color", "red");
-            }
-            button.addClickListener(click -> {
-                // Handle button click here
-            });
-            return button;
-
-        }).setHeader("Settled");
-        grid.addColumn(Scaffold::getResultSquareMeters).setHeader("Square Meters");
-
-        grid.addComponentColumn(item -> {
-            Button button = new Button("MORE");
-
-            button.getStyle().set("background-color", "green");
-            button.getStyle().set("color", "white");
-
-            button.addClickListener(click -> {
-                Dialog dialog = new Dialog();
-                TextArea dialogTextArea = new TextArea("Data:");
-                dialogTextArea.setReadOnly(true);
-                VerticalLayout dialogLayout = new VerticalLayout();
-
-                dialogTextArea.setValue(item.getAdditionalInfo());
-                dialog.setHeaderTitle("Additional Information");
-                dialog.setWidth("600px");
-                dialog.setHeight("400px");
-
-                dialogLayout.add(dialogTextArea);
-                dialogLayout.setAlignItems(Alignment.STRETCH);
-                dialog.add(dialogLayout);
-
-                Button close = new Button("Close", e -> {
-                    dialog.close();
-                });
-                dialog.getFooter().add(close);
-                dialog.open();
-            });
-            return button;
-
-        }).setHeader("Additional Info");
-
-
-        grid.addComponentColumn(item -> {
-            Button delete = new Button(VaadinIcon.TRASH.create());
-            //<theme-editor-local-classname>
-            delete.addClassName("home-view-button-1");
-            delete.getStyle().set("background-color", "red");
-            delete.getStyle().set("color", "white");
-            delete.addClickListener(e -> {
-                //Dodaj potwierdzenie TAK/NIE USUN Z BAZY
-                Notification.show("Deleted!");
-            });
-            return delete;
-        });
-
-
-        return grid;
-
-    }
 
     private static VerticalLayout userHandler() {
         TextField user = new TextField("Custom User");
+        user.addClassName("home-view-text-field-1");
         ComboBox<String> comboBox = new ComboBox<>("Last Users");
+        comboBox.addClassName("home-view-combo-box-1");
         Button setUser = new Button("SET USER");
         setUser.setWidth("190px");
-        setUser.getStyle().set("background-color", "green");
+        setUser.getStyle().set("background-color", "#4e8752");
         setUser.getStyle().set("color", "white");
         setUser.addClickListener(e -> {
             //Dodaj zapisywanie do listy obiektu ilosc x modul
@@ -167,14 +89,19 @@ public class HomeView extends VerticalLayout implements AppShellConfigurator {
         comboBox.setAllowCustomValue(true);
         comboBox.setItems(items);
 
-        return new VerticalLayout(comboBox, user,setUser);
+        return new VerticalLayout(comboBox, user, setUser);
     }
 
     private static VerticalLayout sizesBox() {
         ComboBox<String> comboBox = new ComboBox<>("Sizes");
+        //<theme-editor-local-classname>
+        comboBox.setOverlayClassName("home-view-combo-box-1");
+        //<theme-editor-local-classname>
+        comboBox.addClassName("home-view-combo-box-1");
 
         RadioButtonGroup<Integer> radioButtonGroup = new RadioButtonGroup<>();
         radioButtonGroup.setLabel("Quantity");
+        radioButtonGroup.addClassName("home-view-combo-box-1");
         radioButtonGroup.setItems(
                 IntStream.rangeClosed(1, 6)
                         .boxed()
@@ -183,7 +110,7 @@ public class HomeView extends VerticalLayout implements AppShellConfigurator {
 
         Button add = new Button(VaadinIcon.PLUS.create());
         add.setWidth("190px");
-        add.getStyle().set("background-color", "green");
+        add.getStyle().set("background-color", "#4e8752");
         add.getStyle().set("color", "white");
         add.addClickListener(e -> {
             //Dodaj zapisywanie do listy obiektu ilosc x modul
@@ -198,7 +125,7 @@ public class HomeView extends VerticalLayout implements AppShellConfigurator {
                 .map(e -> String.valueOf(e.getSize()))
                 .toList());
 
-        return new VerticalLayout(comboBox,radioButtonGroup,add);
+        return new VerticalLayout(comboBox, radioButtonGroup, add);
     }
 
     private static VerticalLayout framesBoxes() {
@@ -209,13 +136,13 @@ public class HomeView extends VerticalLayout implements AppShellConfigurator {
 
         Button setFrames = new Button("SET FRAMES");
         setFrames.setWidth("190px");
-        setFrames.getStyle().set("background-color", "green");
+        setFrames.getStyle().set("background-color", "#4e8752");
         setFrames.getStyle().set("color", "white");
         setFrames.addClickListener(e -> {
             //Dodaj zapisywanie do listy obiektu ilosc x modul
             Notification.show("User added!");
         });
-        return new VerticalLayout(checkboxGroup,setFrames);
+        return new VerticalLayout(checkboxGroup, setFrames);
     }
 
     private static VerticalLayout height() {
@@ -227,13 +154,15 @@ public class HomeView extends VerticalLayout implements AppShellConfigurator {
                         .boxed()
                         .collect(Collectors.toList())
         );
-        radioButtonGroup.addValueChangeListener(event->
+        radioButtonGroup.addValueChangeListener(event ->
         {
-          //  currentCalculation = radioButtonGroup.getValue();
+            //  currentCalculation = radioButtonGroup.getValue();
 
         });
 
         TextField customHeight = new TextField("Custom Height");
+        //<theme-editor-local-classname>
+        customHeight.addClassName("home-view-text-field-1");
         return new VerticalLayout(radioButtonGroup, customHeight);
     }
 
@@ -246,7 +175,7 @@ public class HomeView extends VerticalLayout implements AppShellConfigurator {
 
         // Add a value change listener
         sizeTextField.addValueChangeListener(event -> {
-           // sizeLabel.setValue(String.valueOf(currentCalculation));  // Update the Label
+            // sizeLabel.setValue(String.valueOf(currentCalculation));  // Update the Label
         });
         return new HorizontalLayout(sizeTextField, sizeLabel);
     }
