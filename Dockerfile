3b942bdef2b7252dbabf3dc057f75abc2f86434c
Owidiusz Zieliński <owi19955@gmail.com>
2024-02-19 22:25:28 +0100

    test changes docker deploy

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM eclipse-temurin:17-jre
-COPY target/*.jar app.jar
+FROM maven:3.8-openjdk-17-slim AS build
+COPY . /home/app
+WORKDIR /home/app
+RUN mvn -f /home/app/pom.xml clean package -Pproduction -DskipTests -X
+
+
+FROM openjdk:17-alpine
+COPY --from=build /home/app/target/scaff-done-0.0.2-SNAPSHOT.jar /usr/local/lib/app.jar
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "/app.jar"]
+ENTRYPOINT ["java", "-jar", "/usr/local/lib/app.jar"]
--- frontend/generated/flow/generated-flow-imports.js
+++ frontend/generated/flow/generated-flow-imports.js
@@ -11,11 +11,11 @@ import '@vaadin/vaadin-lumo-styles/vaadin-iconset.js';
 
 const loadOnDemand = (key) => {
   const pending = [];
-  if (key === 'ff0ebb1203ed99b591b434b1cebe2015395a622cc82a6b96657006c6cf8e2862') {
-    pending.push(import('./chunks/chunk-8d3a863b905e9a9b4ef1f679dab2b53c65bfc3db5a1d573446866def9400bd3b.js'));
-  }
   if (key === 'b1bee3b5a548920513e695c9660d1d3d1b38851adbcb57bee965d2ec5b9d2118') {
     pending.push(import('./chunks/chunk-fb6fd68dadae929c98929f93487254597206f38ab568782fbabe69e258a4ee09.js'));
+  }
+  if (key === 'ff0ebb1203ed99b591b434b1cebe2015395a622cc82a6b96657006c6cf8e2862') {
+    pending.push(import('./chunks/chunk-8d3a863b905e9a9b4ef1f679dab2b53c65bfc3db5a1d573446866def9400bd3b.js'));
   }
   return Promise.all(pending);
 }
